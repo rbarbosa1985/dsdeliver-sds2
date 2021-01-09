f7a45d175da9a2d14500abd2534ff828b9978b51
Roberto Barbosa <betoanselmo@gmail.com>
2021-01-09 00:41:34 -0300

    initial setup

--- backend/src/main/java/com/devsuperior/dsdeliver/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.devsuperior.dsdeliver.config;
 
-import java.util.Arrays;
+import java.util.Arrays;	
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
