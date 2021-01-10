61923381f39e273e5e2133cc138084ba79dfd6e1
Roberto Barbosa <betoanselmo@gmail.com>
2021-01-10 15:56:55 -0300

    Cors

--- backend/src/main/java/com/devsuperior/dsdeliver/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.devsuperior.dsdeliver.config;
 
-import java.util.Arrays;	
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -12,6 +14,7 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
--- front-web/src/Orders/api.ts
+++ front-web/src/Orders/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://roberto-sds2.herokuapp.com/'
+const API_URL = 'https://roberto-sds2.herokuapp.com'
 
 export function fetchProducts () {
      return axios(`${API_URL}/products`)
