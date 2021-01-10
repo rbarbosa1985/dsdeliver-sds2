cc3351f7f77aef474f66b113625d467e98c28479
Roberto Barbosa <betoanselmo@gmail.com>
2021-01-10 12:22:52 -0300

    add footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './styles.css';
+import { ReactComponent as InstagranIcon} from './instagram.svg';
+import { ReactComponent as YouTubeIcon} from './youtube.svg';
+import { ReactComponent as LinkedinIcon} from './linkedin.svg';
+
+function Footer() {
+     return (
+          <footer className="main-footer">
+               App desenvolvido durante a 2ª ed. do evento Semana DevSuperior
+               <div className="footer-icons">
+                    <a href="https://www.youtube.com/c/DevSuperior" target="_new"><YouTubeIcon/></a>
+                    <a href="https://www.linkedin.com/school/devsuperior/" target="_blank"><LinkedinIcon/></a>
+                    <a href="https://www.instagram.com/devsuperior.ig/" ><InstagranIcon/></a>
+                </div>
+          </footer>
+     )
+}
+
+export default Footer;
